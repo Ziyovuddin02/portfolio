5b46a51bee79c158a11084f767a8b81fd5443f76
Ziyovuddin02 <ziyovuddinbekmuratov87@gmail.com>
2025-03-10 09:48:22 +0500

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -16,374 +16,121 @@ import { Date, Github, Dribble, Product } from '../components/Icons';
 
 const About = () => {
   return (
-    <Layout title="About Me">
+    <Layout title="Men Haqimda">
       <PageSection>
-        <PageWrapper
-          className="mb-5"
-          aria-label="You are now in my educational background section"
-        >
-          <h1 className="intro__text">About Me.</h1> <br />
+        <PageWrapper className="mb-5" aria-label="Men haqimdagi ma'lumotlar">
+          <h1 className="intro__text">Men Haqimda.</h1> <br />
           <article>
             <ul className="timeline">
               <li className="mt-2">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Github Link"
-                  href="https://github.com/adenekan41"
-                >
-                  Engineering{' '}
+                <Link href="/projects">
+                  Frontend Dasturchi{' '}
                   <small>
                     <Github />
                   </small>
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Github Link"
+                </Link>
+                <Link
+                  href="/projects"
                   id="cardHover"
-                  href="https://github.com/adenekan41"
                   className="float-right"
                 >
-                  View Github
-                </a>
+                  Loyihalarni Ko'rish
+                </Link>
                 <p>
-                  The power of first impressions cannot be underestimated, and
-                  the gateway to capitalizing on them lies in exceptional
-                  website design. An outstanding website transcends mere
-                  aesthetics and extends its influence to encompass seamless
-                  functionality and user-friendly navigation. Drawing upon my
-                  expertise as a seasoned programmer, I possess the unique
-                  ability to tackle intricate technical challenges while
-                  crafting websites that exude sleekness and visual allure.
-                  Moreover, my extensive knowledge of recognized technical
-                  standards is complemented by my proficiency in modern building
-                  practices, ensuring that every aspect of your website is
-                  finely tuned to perfection.
+                  Men Frontend dasturchiman, qo'shimcha ravishda Backend va AI bilan ishlash tajribasiga egaman. Hozirda turli korxonalarga web-saytlar, Telegram botlar va keng qamrovli IT xizmatlarini muvaffaqiyatli taqdim etib kelmoqdaman.  
                 </p>
               </li>
               <li>
-                <Link href="/projects" aria-label="Open Products Page">
-                  Product{' '}
+                <Link href="/projects">
+                  O'qituvchi{' '}
                   <small>
                     <Product />
                   </small>
                 </Link>
                 <Link
                   href="/projects"
-                  aria-label="Open Products Page"
                   id="cardHover"
                   className="float-right"
                 >
-                  View Products
+                  O'quvchilarim
                 </Link>
                 <p>
-                  While I may not fit the conventional mold of a product
-                  manager, my diverse skill set in research, product design, and
-                  product coordination empowers me to drive the growth of a
-                  product from its inception. As an exceptional analytical
-                  thinker, I possess the ability to uphold the product's vision
-                  throughout its entire journey, effectively bridging the
-                  technical and product aspects. By leveraging my expertise, I
-                  can navigate the path from 0 to 1, ensuring the product's
-                  success at every stage.
+                  Jadidlar o'quv markazi va Ziyo Ta'lim markazlarida boshlang'ich dasturlash bilimlarini o'rgataman. O'quvchilarga o'z bilim va ko'nikmalarimni ulashish menga zavq bag'ishlaydi.
                 </p>
               </li>
-
               <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Design Link"
-                  href="https://dribbble.com/codewonders"
-                >
-                  Design{' '}
+                <Link href="/contact">
+                  Bog'lanish{' '}
                   <small>
                     <Dribble />
                   </small>
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Design Link"
-                  id="cardHover"
-                  href="https://dribbble.com/codewonders"
-                  className="float-right"
-                >
-                  View Dribbble
-                </a>
-                <p>
-                  Despite not fitting the typical designer stereotype, my
-                  exceptional visual abilities enable me to excel as a
-                  presenter, effectively conveying design concepts to
-                  stakeholders and design teams. I possess a remarkable aptitude
-                  for conceptualization, allowing me to envision and bring forth
-                  stunning creations. Additionally, my expertise lies in
-                  fine-tuning stylesheets and crafting seamless user experiences
-                  that flow effortlessly.
-                </p>
-              </li>
-            </ul>
-          </article>
-        </PageWrapper>
-
-        <PageWrapper
-          className="mb-5"
-          aria-label="You are now in my musical playlist section"
-        >
-          <article>
-            <h4>My Reads.</h4>
-            <ul className="timeline">
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  id="cardHover"
-                  aria-label="Open Human Centered Design 101"
-                  href="https://www.plusacumen.org/courses/introduction-human-centered-design"
-                >
-                  Human Centered Design 101. <small>Acumen</small>
-                </a>
-                <a className="float-right" tabIndex={-1}>
-                  <Date /> <b>2017 - 2018</b>
-                </a>
-                <p>
-                  This is an intensive, hands-on learning experience that will
-                  challenge you to get out of your chair and out into the real
-                  world to talk to people and test your ideas. You’ll leave this
-                  experience equipped and energized to apply the human-centered
-                  design process to challenges across industries, sectors, and
-                  geographies to generate breakthrough ideas.
-                </p>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  id="cardHover"
-                  aria-label="Open Link Data Structure And Algorithm"
-                  href="https://www.udemy.com/course/master-the-coding-interview-data-structures-algorithms/"
-                >
-                  Data Structure And Algorithm. <small>Udemy</small>
-                </a>
-                <a className="float-right" tabIndex={-1}>
-                  <Date /> <b>2019</b>
-                </a>
-                <p>
-                  Many developers who are &quot;self taught&quot;, feel that one
-                  of the main disadvantages they face compared to college
-                  educated graduates in computer science is the fact that they
-                  don&apos;t have knowledge about algorithms, data structures
-                  and the notorious Big-O Notation
-                </p>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  id="cardHover"
-                  aria-label="Open Link Adaptive Leadership"
-                  href="https://www.edx.org/course/adaptive-leadership-in-development"
-                >
-                  Adaptive Leadership. <small>edX</small>
-                </a>
-                <a className="float-right" tabIndex={-1}>
-                  <Date /> <b>2019</b>
-                </a>
-                <p>
-                  This introductory course helps you identify and make progress
-                  on a leadership challenge in your own life, team, or
-                  community. You’ll apply the tools and techniques of Adaptive
-                  Leadership, a practical framework developed at Harvard Kennedy
-                  School of Government for leading change, particularly during
-                  times of uncertainty or when there are no easy answers.{' '}
-                </p>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  id="cardHover"
-                  aria-label="Open Link Learning to Learn [Efficient Learning] "
-                  href="https://www.udemy.com/course/learning-to-learn-efficient-learning-zero-to-mastery/"
-                >
-                  Learning to Learn [Efficient Learning] <small>udemy</small>
-                </a>
-                <a className="float-right" tabIndex={-1}>
-                  <Date /> <b>2020</b>
-                </a>
-                <p>
-                  Improve memory & productivity using the skills of the world's
-                  top performers and learning strategies proven by research.
-                </p>
-              </li>
-              <li>
+                </Link>
                 <Link
-                  href="/articles"
-                  aria-label="Check my articles and recent reads"
+                  href="/contact"
                   id="cardHover"
+                  className="float-right"
                 >
-                  Learning Something Great 😉
+                  Murojaat Qilish
                 </Link>
-                <a className="float-right" tabIndex={-1}>
-                  <Date /> <b>Currently</b>
-                </a>
-                <p>We continue learning everyday. </p>
+                <p>
+                  Agar ko'proq ma'lumot olishni istasangiz yoki biror loyiha ustida ishlashni xohlasangiz, bog'laning. Siz bilan hamkorlik qilishdan mamnun bo'laman! 
+                </p>
               </li>
             </ul>
           </article>
         </PageWrapper>
 
-        <PageWrapper
-          className="mb-5"
-          aria-label="You are now in my musical playlist section"
-        >
+        <PageWrapper className="mb-5" aria-label="Talim">
           <article>
-            <h4>My Playlist.</h4>
+            <h4>Ta'lim.</h4>
             <ul className="timeline">
               <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Codewonders Spotify Playlist"
-                  href="https://open.spotify.com/playlist/2EH5NURI9OzRsMOFOGL70V"
-                >
-                  Codewonders. <small>Spotify</small>
-                </a>
-                <a
-                  target="_blank"
-                  id="cardHover"
-                  rel="noopener noreferrer"
-                  aria-label="Open Lofii Playlist"
-                  href="https://open.spotify.com/playlist/2EH5NURI9OzRsMOFOGL70V"
-                  className="float-right"
-                >
-                  View Playlist
+                <Link href="https://tuit.uz/">
+                  Toshkent Axborot Texnologiyalari Universiteti{' '}
+                  <small>2021 - 2025</small>
+                </Link>
+                <a className="float-right">
+                  <Date /> <b>Bakalavr</b>
                 </a>
                 <p>
-                  This is an intensive, energizing songs list that helps me
-                  through the day and inspires generate breakthrough ideas.
+                  Kompyuter injiniringi yo'nalishi bo'yicha tahsil olyapman.
                 </p>
               </li>
               <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Link"
-                  href="https://open.spotify.com/playlist/37i9dQZF1DX8Uebhn9wzrS"
-                >
-                  Chill Lofi Study Beats <small>Spotify</small>
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Link"
-                  id="cardHover"
-                  href="https://open.spotify.com/playlist/37i9dQZF1DX8Uebhn9wzrS"
-                  className="float-right"
-                >
-                  View Playlist
+                <Link href="#">
+                  Chirchiq davlat pedagogika universiteti qoshidagi texnikum{' '}
+                  <small>2020 - 2022</small>
+                </Link>
+                <a className="float-right">
+                  <Date /> <b>Darajasiz</b>
                 </a>
                 <p>
-                  The perfect study beats, twenty four seven. with over 178
-                  SONGS
+                  Kompyuter tizimlari va dasturlash yo'nalishi bo'yicha o'qiganman.
                 </p>
               </li>
               <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Link"
-                  href="https://open.spotify.com/playlist/37i9dQZF1DWUACcBjzMiIY?si=KbZW7Cz1TpaABPXV_fH4-A"
-                >
-                  Mellow Drive <small>Spotify</small>
+                <Link href="#">
+                  3-sonli umumiy o'rta ta'lim maktabi{' '}
+                  <small>2017 - 2020</small>
+                </Link>
+                <a className="float-right">
+                  <Date /> <b>Diplom</b>
                 </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Link"
-                  id="cardHover"
-                  href="https://open.spotify.com/playlist/37i9dQZF1DWUACcBjzMiIY?si=KbZW7Cz1TpaABPXV_fH4-A"
-                  className="float-right"
-                >
-                  View Playlist
-                </a>
-                <p>Easy listening for pleasant drive</p>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Link"
-                  href="https://open.spotify.com/playlist/37i9dQZF1DX6PKX5dyBKeq?si=jRd8iiIER260sRzE6ZV44g"
-                >
-                  Rap UK <small>Spotify</small>
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Link"
-                  id="cardHover"
-                  href="https://open.spotify.com/playlist/37i9dQZF1DX6PKX5dyBKeq?si=jRd8iiIER260sRzE6ZV44g"
-                  className="float-right"
-                >
-                  View Playlist
-                </a>
-                <p>One of the finest rap in UK. Cover: Aitch</p>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Link"
-                  href="https://open.spotify.com/playlist/37i9dQZF1DX3rxVfibe1L0?si=wF0gpf3RRa-2oT1GYTBQ_w"
-                >
-                  Mood Booster 🎅🏽 <small>Spotify</small>
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Open Link"
-                  id="cardHover"
-                  href="https://open.spotify.com/playlist/37i9dQZF1DX3rxVfibe1L0?si=wF0gpf3RRa-2oT1GYTBQ_w"
-                  className="float-right"
-                >
-                  View Playlist
-                </a>
-                <p>Get happy with today's dose of feel-good songs!</p>
+                <p>
+                  Maktabda informatika va kompyuter fanlariga qiziqishim ortdi.
+                </p>
               </li>
             </ul>
           </article>
         </PageWrapper>
 
-        <PageWrapper aria-label="You are now in lens section">
-          <article>
-            <h4>My Photochromic Lens.</h4>
-            <ul className="timeline">
-              <li>
-                <Link href="/lens">
-                  Adenekan's Collection. <small>©Adenekan</small>
-                </Link>
-                <Link href="/lens" className="float-right" id="cardHover">
-                  Visit Page
-                </Link>
-                <p>
-                  This is an intensive, curation of the photochromic lenses that
-                  standout and the ones that i personally love, NOTE this is
-                  just by preference actually.
-                </p>
-              </li>
-            </ul>
-          </article>
+        <PageWrapper>
+          <FooterLink goto="/projects" className="mt-3 mb-5">
+            Loyihalarga O'tish
+          </FooterLink>
+          <br />
         </PageWrapper>
       </PageSection>
-
-      <PageWrapper>
-        <FooterLink goto="/projects" className="mt-3 mb-5">
-          Lets Continue To Projects
-        </FooterLink>
-        <br />
-      </PageWrapper>
     </Layout>
   );
 };
